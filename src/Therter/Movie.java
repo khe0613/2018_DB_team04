f9f831fee34cfa77b5830f27e6f102d433c877ae
LeeHaeWon <banner4@cnu.ac.kr>
2018-11-20 20:07:03 +0900

    첫 시작

--- src/Therter/Movie.java
+++ src/Therter/Movie.java
@@ -1,6 +1,14 @@
 package Therter;
 
-
 public class Movie {
-
+	public static void printMessage(String str) {
+		System.out.println(str);
+	}
+	
+	public static void main(String args[]) {
+		printMessage("---------- Database TermProject 04조 ----------");
+		printMessage("------------- 영  화  관  리  시  스  템  ----------------");
+	}
 }
+
+
